54a4f78181ef4e86a34af9d96ac8e5dad97649ac
Deathlesz <deathless.mcd@gmail.com>
2026-03-11 17:04:45 +0300

    fix: try to fix again

--- today.py
+++ today.py
@@ -484,13 +484,13 @@ def svg_overwrite(
     """
     tree = etree.parse(filename)
     root = tree.getroot()
-    justify_format(root, "commit_data", commit_data, 22)
-    justify_format(root, "star_data", star_data, 15)
-    justify_format(root, "repo_data", repo_data, 7)
+    justify_format(root, "commit_data", commit_data, 23)
+    justify_format(root, "star_data", star_data, 16)
+    justify_format(root, "repo_data", repo_data, 8)
     justify_format(root, "contrib_data", contrib_data)
-    justify_format(root, "follower_data", follower_data, 11)
-    justify_format(root, "loc_data", loc_data[2], 9)
-    justify_format(root, "loc_add", loc_data[0], 1)
+    justify_format(root, "follower_data", follower_data, 12)
+    justify_format(root, "loc_data", loc_data[2], 10)
+    justify_format(root, "loc_add", loc_data[0])
     justify_format(root, "loc_del", loc_data[1], 6)
     tree.write(filename, encoding="utf-8", xml_declaration=True)
 
@@ -508,7 +508,7 @@ def justify_format(root, element_id, new_text, length=0):
         dot_map = {0: "", 1: " ", 2: ". "}
         dot_string = dot_map[just_len]
     else:
-        dot_string = " " + ("." * just_len) + " "
+        dot_string = " " + ("." * (just_len - 1)) + " "
     find_and_replace(root, f"{element_id}_dots", dot_string)
 
 
